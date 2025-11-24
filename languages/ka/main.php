17912c27cca1ed6b2bac9116bf49342162240c62
Yury Pikhtarev <exileum@icloud.com>
2025-11-25 00:10:53 +0400

    New translations main.php (Georgian)

--- languages/ka/main.php
+++ languages/ka/main.php
@@ -2458,6 +2458,12 @@ $lang['TR_STATS'] = [
     7 => 'რამდენი მართლა ღმერთი ბოლო 30 დღის განმავლობაში?',
 ];
 
+$lang['TORRENT_STATS_TITLE'] = 'Torrent Statistics';
+$lang['TRACKER_STATS_TITLE'] = 'Tracker Statistics';
+$lang['TORRENTS'] = 'Torrents';
+$lang['PEERS'] = 'Peers';
+$lang['CLIENTS'] = 'Clients';
+
 $lang['NEW_POLL_START'] = 'გამოკითხვა ჩართულია';
 $lang['NEW_POLL_END'] = 'გამოკითხვა დასრულდა';
 $lang['NEW_POLL_ENDED'] = 'ამ გამოკითხვაში უკვე დასრულებულია';
