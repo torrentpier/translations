f4f42e29f6e7dc05963bfae755fa1d834a78882e
Yury Pikhtarev <exileum@icloud.com>
2025-11-25 00:12:04 +0400

    New translations main.php (Uzbek)

--- languages/uz/main.php
+++ languages/uz/main.php
@@ -2458,6 +2458,12 @@ $lang['TR_STATS'] = [
     7 => 'Oxirgi 30 kun ichida qancha yuklovchilar.',
 ];
 
+$lang['TORRENT_STATS_TITLE'] = 'Torrent Statistics';
+$lang['TRACKER_STATS_TITLE'] = 'Tracker Statistics';
+$lang['TORRENTS'] = 'Torrents';
+$lang['PEERS'] = 'Peers';
+$lang['CLIENTS'] = 'Clients';
+
 $lang['NEW_POLL_START'] = 'Ovoz yoqilgan.';
 $lang['NEW_POLL_END'] = 'Ovoz yakunlandi.';
 $lang['NEW_POLL_ENDED'] = 'Bu so\'rov allaqachon yakunlandi';
