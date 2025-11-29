8b0d8885b9d47f9097a344c4e307355e756d6205
Yury Pikhtarev <exileum@icloud.com>
2025-11-30 00:53:56 +0400

    New translations main.php (Greek)

--- languages/el/main.php
+++ languages/el/main.php
@@ -782,8 +782,8 @@ $lang['SORRY_AUTH_VIEW_ATTACH'] = 'Λυπάμαι αλλά δεν είστε ε�
 $lang['DESCRIPTION'] = 'Περιγραφή.'; // used in Administration Panel too...
 $lang['DOWNLOAD'] = 'Κατεβάστε το'; // this Language Variable is defined in admin.php too, but we are unable to access it from the main Language File
 $lang['FILESIZE'] = 'Το μέγεθος αρχείου';
-$lang['MAX_FILE_SIZE'] = 'Max file size';
-$lang['ALLOWED_EXTENSIONS'] = 'Allowed extensions';
+$lang['MAX_FILE_SIZE'] = 'Μέγιστο μέγεθος αρχείου';
+$lang['ALLOWED_EXTENSIONS'] = 'Επιτρεπτές επεκτάσεις';
 
 // Viewtopic -> Display of Attachments -> TorrServer integration
 $lang['STREAM'] = 'Ροή';
@@ -802,15 +802,15 @@ $lang['COPY_STREAM_LINK'] = 'Αντιγραφή συνδέσμου ροής στ
 $lang['M3U_NOT_SUPPORTED'] = 'Αυτό το αρχείο δεν μπορεί να αναπαραχθεί στον περιηγητή...';
 $lang['M3U_FFPROBE_NO_DATA'] = 'Φαίνεται ότι το ffprobe δεν θα μπορέσει να επιστρέψει δεδομένα σχετικά με αυτόν τον κωδικοποιητή...';
 $lang['M3U_NOTICE'] = 'Ορισμένοι περιηγητές δεν υποστηρίζουν την αναπαραγωγή ορισμένων μορφών βίντεο. Σε αυτήν την περίπτωση, μπορείτε να κατεβάσετε το αρχείο .m3u και να το παίξετε χρησιμοποιώντας έναν παίκτη τρίτου μέρους';
-$lang['TORRSERVER_UNAVAILABLE'] = 'TorrServer is unavailable';
-$lang['TORRSERVER_INVALID_REQUEST'] = 'Invalid request parameters';
+$lang['TORRSERVER_UNAVAILABLE'] = 'Το TorrServer δεν είναι διαθέσιμο';
+$lang['TORRSERVER_INVALID_REQUEST'] = 'Μη έγκυρα παραμέτρους αιτήματος';
 
 $lang['ATTACHMENT'] = 'Συνημμένα';
 
 // Posting -> Posting Attachments
 $lang['ADD_ATTACHMENT'] = 'Προσθήκη Συνημμένου';
 $lang['ADD_ATTACHMENT_EXPLAIN'] = 'Αν δεν θέλετε να προσθέσετε κάποιο Συνημμένο στο μήνυμα σας, παρακαλώ αφήστε τα πεδία κενά.';
-$lang['UPDATE_ATTACHMENT'] = 'Update Attachment';
+$lang['UPDATE_ATTACHMENT'] = 'Ενημέρωση συνημμένου';
 $lang['FILENAME'] = 'Όνομα αρχείου';
 
 // Posting -> Posted Attachments
@@ -820,7 +820,7 @@ $lang['DELETE_ATTACHMENT'] = 'Διαγραφή Συνημμένου';
 // Errors -> Download
 $lang['NO_ATTACHMENT_SELECTED'] = 'Δεν έχετε επιλέξει ένα συνημμένο για να το κατεβάσετε ή να δείτε.';
 $lang['ERROR_NO_ATTACHMENT'] = 'Το επιλεγμένο Συνημμένο δεν υπάρχει πια';
-$lang['DOWNLOAD_LIMIT_EXCEEDED'] = 'Daily download limit exceeded. Please try again tomorrow.';
+$lang['DOWNLOAD_LIMIT_EXCEEDED'] = 'Υπέρβαση ημερήσιου ορίου λήψεων. Παρακαλώ δοκιμάστε ξανά αύριο.';
 
 //bt
 $lang['BT_REG_YES'] = 'Εγγεγραμμένοι';
@@ -1152,8 +1152,8 @@ $lang['COLLAPSE'] = 'Κατάρρευση καταλόγου.';
 $lang['EXPAND'] = 'Αναπτύξτε το αρχείο.';
 $lang['SWITCH'] = 'Εναλλαγή.';
 $lang['TOGGLE_WINDOW_FULL_SIZE'] = 'Αύξηση/μείωση του παραθύρου';
-$lang['EMPTY_TOPIC_ID'] = 'Missing topic identifier';
-$lang['TOR_NOT_FOUND'] = 'File is missing from the server';
+$lang['EMPTY_TOPIC_ID'] = 'Λείπει το αναγνωριστικό θέματος';
+$lang['TOR_NOT_FOUND'] = 'Το αρχείο λείπει από τον διακομιστή';
 $lang['ERROR_BUILD'] = 'Το περιεχόμενο αυτού του torrent αρχείου δεν μπορεί να προβληθεί στο site.';
 $lang['TORFILE_INVALID'] = 'Το torrent αρχείο είναι κατεστραμμένο.';
 
