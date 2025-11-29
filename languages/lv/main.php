74fd5ee050b6bb21fb33b0175051df5a7d305d3c
Yury Pikhtarev <exileum@icloud.com>
2025-11-30 00:54:32 +0400

    New translations main.php (Latvian)

--- languages/lv/main.php
+++ languages/lv/main.php
@@ -782,8 +782,8 @@ $lang['SORRY_AUTH_VIEW_ATTACH'] = 'Atvainojiet, bet jums nav atļauts apskatīt 
 $lang['DESCRIPTION'] = 'Apraksts'; // used in Administration Panel too...
 $lang['DOWNLOAD'] = 'Lejupielādēt'; // this Language Variable is defined in admin.php too, but we are unable to access it from the main Language File
 $lang['FILESIZE'] = 'Faila izmērs';
-$lang['MAX_FILE_SIZE'] = 'Max file size';
-$lang['ALLOWED_EXTENSIONS'] = 'Allowed extensions';
+$lang['MAX_FILE_SIZE'] = 'Maksimālais faila izmērs';
+$lang['ALLOWED_EXTENSIONS'] = 'Atļautie paplašinājumi';
 
 // Viewtopic -> Display of Attachments -> TorrServer integration
 $lang['STREAM'] = 'Straume';
@@ -802,15 +802,15 @@ $lang['COPY_STREAM_LINK'] = 'Kopēt straumes saiti starpliktuvē';
 $lang['M3U_NOT_SUPPORTED'] = 'Šo failu nevar atskaņot pārlūkprogrammā...';
 $lang['M3U_FFPROBE_NO_DATA'] = 'Izskatās, ka ffprobe nebūs iespējams atgriezt datus par šo kodeku...';
 $lang['M3U_NOTICE'] = 'Dažas pārlūkprogrammas neatbalsta noteiktu video formātu atskaņošanu. Šādā gadījumā varat lejupielādēt .m3u failu un atskaņot to, izmantojot trešās puses atskaņotāju';
-$lang['TORRSERVER_UNAVAILABLE'] = 'TorrServer is unavailable';
-$lang['TORRSERVER_INVALID_REQUEST'] = 'Invalid request parameters';
+$lang['TORRSERVER_UNAVAILABLE'] = 'TorrServer nav pieejams';
+$lang['TORRSERVER_INVALID_REQUEST'] = 'Nederīgi pieprasījuma parametri';
 
 $lang['ATTACHMENT'] = 'Pielikumi';
 
 // Posting -> Posting Attachments
 $lang['ADD_ATTACHMENT'] = 'Pievienot pielikumu';
 $lang['ADD_ATTACHMENT_EXPLAIN'] = 'Ja nevēlaties pievienot pielikumu savai publikācijai, lūdzu, atstājiet laukus tukšus.';
-$lang['UPDATE_ATTACHMENT'] = 'Update Attachment';
+$lang['UPDATE_ATTACHMENT'] = 'Atjaunināt pielikumu';
 $lang['FILENAME'] = 'Faila nosaukums';
 
 // Posting -> Posted Attachments
@@ -820,7 +820,7 @@ $lang['DELETE_ATTACHMENT'] = 'Dzēst pielikumu.';
 // Errors -> Download
 $lang['NO_ATTACHMENT_SELECTED'] = 'Jūs neesat izvēlēšanai pielikuma, lai lejupielādētu vai apskatītu.';
 $lang['ERROR_NO_ATTACHMENT'] = 'Izvēlētais pielikums vairs nepastāv.';
-$lang['DOWNLOAD_LIMIT_EXCEEDED'] = 'Daily download limit exceeded. Please try again tomorrow.';
+$lang['DOWNLOAD_LIMIT_EXCEEDED'] = 'Pārsniegts ikdienas lejupielādes limits. Lūdzu, mēģiniet vēlreiz rīt.';
 
 //bt
 $lang['BT_REG_YES'] = 'Reģistrēts';
@@ -1152,8 +1152,8 @@ $lang['COLLAPSE'] = 'Sabrukt katalogs';
 $lang['EXPAND'] = 'Paplašināt';
 $lang['SWITCH'] = 'Pārslēgt';
 $lang['TOGGLE_WINDOW_FULL_SIZE'] = 'Palielināt/mazināt logu';
-$lang['EMPTY_TOPIC_ID'] = 'Missing topic identifier';
-$lang['TOR_NOT_FOUND'] = 'File is missing from the server';
+$lang['EMPTY_TOPIC_ID'] = 'Trūkst tēmas identifikators';
+$lang['TOR_NOT_FOUND'] = 'Fails trūkst serverī';
 $lang['ERROR_BUILD'] = 'Šī torrent faila saturs nevar tikt skatīts vietnē (neizdevās izveidot failu sarakstu)';
 $lang['TORFILE_INVALID'] = 'Torrent fails ir bojāts';
 
