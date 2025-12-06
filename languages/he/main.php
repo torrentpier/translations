2baa22ed7ada185eeb33fc3140cafb482b7977bc
Yury Pikhtarev <exileum@icloud.com>
2025-12-07 01:15:42 +0300

    New translations main.php (Hebrew)

--- languages/he/main.php
+++ languages/he/main.php
@@ -142,7 +142,7 @@ $lang['NOTHING_HAS_CHANGED'] = 'שום דבר לא השתנה';
 
 // Stats block text
 $lang['POSTED_TOPICS_TOTAL'] = 'המשתמשים שלנו פרסמו סך של <b>%s</b> נושאים'; // Number of topics
-$lang['POSTED_ARTICLES_TOTAL'] = 'המשתמשים שלנו פרסמו סך של <b>%s</b> מאמרים'; // Number of posts
+$lang['POSTED_POSTS_TOTAL'] = 'Our users have posted a total of <b>%s</b> posts'; // Number of posts
 $lang['REGISTERED_USERS_TOTAL'] = 'יש לנו <b>%s</b> משתמשים רשומים'; // # registered users
 $lang['USERS_TOTAL_GENDER'] = 'בנים: <b>%d</b>, בנות: <b>%d</b>, אחרים: <b>%d</b>';
 $lang['NEWEST_USER'] = 'המשתמש שנרשם הכי חדש הוא <b>%s</b>.'; // a href, username, /a
