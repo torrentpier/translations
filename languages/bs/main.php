8afc445133c5f6f4fa91d497d8d92d553771c597
Yury Pikhtarev <exileum@icloud.com>
2025-12-07 00:14:50 +0300

    New translations main.php (Bosnian)

--- languages/bs/main.php
+++ languages/bs/main.php
@@ -1756,6 +1756,11 @@ $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_POLLCREATE'] = '%s može stvoriti ankete u o
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_ATTACHMENTS'] = '%s može postaviti priloge';
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_DOWNLOAD'] = '%s može preuzeti priloge';
 
+// Tooltips for disabled permissions
+$lang['AUTH_TOOLTIP_FORUM_MODE'] = 'Forum mode: %s — cannot be changed';
+$lang['AUTH_TOOLTIP_MOD_OVERRIDE'] = 'Permission enabled via "Moderator"';
+$lang['AUTH_TOOLTIP_GROUP_HAS'] = 'Permission granted by the group';
+
 // Misc
 $lang['SF_SHOW_ON_INDEX'] = 'Prikaži na glavnoj stranici';
 $lang['SF_PARENT_FORUM'] = 'Roditeljski forum';
