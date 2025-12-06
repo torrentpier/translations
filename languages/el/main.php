f41e5e5eee0022c30ef5a1b505f1e52eb51db6ca
Yury Pikhtarev <exileum@icloud.com>
2025-12-07 00:14:18 +0300

    New translations main.php (Greek)

--- languages/el/main.php
+++ languages/el/main.php
@@ -1756,6 +1756,11 @@ $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_POLLCREATE'] = '%s μπορεί να δημ�
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_ATTACHMENTS'] = '%s μπορεί να δημοσιεύσει συνημμένα αρχεία.';
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_DOWNLOAD'] = '%s μπορεί να κατεβάσει τα συνημμένα.';
 
+// Tooltips for disabled permissions
+$lang['AUTH_TOOLTIP_FORUM_MODE'] = 'Forum mode: %s — cannot be changed';
+$lang['AUTH_TOOLTIP_MOD_OVERRIDE'] = 'Permission enabled via "Moderator"';
+$lang['AUTH_TOOLTIP_GROUP_HAS'] = 'Permission granted by the group';
+
 // Misc
 $lang['SF_SHOW_ON_INDEX'] = 'Εμφάνιση στην κεντρική σελίδα.';
 $lang['SF_PARENT_FORUM'] = 'Μητρικό φόρουμ.';
