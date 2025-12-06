c08be9c1f125f9f2efcdc8289b88b092db9ccaba
Yury Pikhtarev <exileum@icloud.com>
2025-12-07 02:18:39 +0300

    New translations main.php (Azerbaijani)

--- languages/az/main.php
+++ languages/az/main.php
@@ -102,7 +102,7 @@ $lang['ADMIN_DISABLE_CRON_TITLE'] = 'Forum texniki xidmətdədir.';
 $lang['ADMIN_UNLOCK'] = 'Forum aktiv et';
 $lang['ADMIN_UNLOCKED'] = 'Açılmışdır';
 $lang['ADMIN_UNLOCK_CRON'] = 'Kilidi çıxar';
-$lang['PAGE_NOT_FOUND'] = 'Page not found';
+$lang['PAGE_NOT_FOUND'] = 'Səhifə tapılmadı';
 
 $lang['LOADING'] = 'Yükləmə...';
 $lang['JUMPBOX_TITLE'] = 'Seçin forum';
@@ -142,7 +142,7 @@ $lang['NOTHING_HAS_CHANGED'] = 'Heç nə dəyişməyib';
 
 // Stats block text
 $lang['POSTED_TOPICS_TOTAL'] = 'Bizim istifadəçilər <b>%s</b> mövzular göndərdilər.'; // Number of topics
-$lang['POSTED_POSTS_TOTAL'] = 'Our users have posted a total of <b>%s</b> posts'; // Number of posts
+$lang['POSTED_POSTS_TOTAL'] = 'İstifadəçilərimiz ümumilikdə <b>%s</b> göndəriş yerləşdirib'; // Number of posts
 $lang['REGISTERED_USERS_TOTAL'] = 'Biz <b>%s</b> qeydiyyatdan keçmiş istifadəçilər'; // # registered users
 $lang['USERS_TOTAL_GENDER'] = 'Oğlan: <b>%d</b>, Qızlar: <b>%d</b>, Digər: <b>%d</b>';
 $lang['NEWEST_USER'] = 'Son qeydiyyatlı istifadəçi <b>%s</b>'; // a href, username, /a
@@ -1757,9 +1757,9 @@ $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_ATTACHMENTS'] = '%s faylları yerləşdirə 
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_DOWNLOAD'] = '%s faylları yükləyə bilər.';
 
 // Tooltips for disabled permissions
-$lang['AUTH_TOOLTIP_FORUM_MODE'] = 'Forum mode: %s — cannot be changed';
-$lang['AUTH_TOOLTIP_MOD_OVERRIDE'] = 'Permission enabled via "Moderator"';
-$lang['AUTH_TOOLTIP_GROUP_HAS'] = 'Permission granted by the group';
+$lang['AUTH_TOOLTIP_FORUM_MODE'] = 'Forum rejimi: %s — dəyişdirilə bilməz';
+$lang['AUTH_TOOLTIP_MOD_OVERRIDE'] = 'İcazə "Moderator" vasitəsilə verilib';
+$lang['AUTH_TOOLTIP_GROUP_HAS'] = 'İcazə qrup tərəfindən verilmişdir';
 
 // Misc
 $lang['SF_SHOW_ON_INDEX'] = 'Ana səhifədə göstər.';
