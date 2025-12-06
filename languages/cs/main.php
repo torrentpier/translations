4b9fc68d85272fc99f6745b2f4f13743f14a2b8e
Yury Pikhtarev <exileum@icloud.com>
2025-12-07 00:14:15 +0300

    New translations main.php (Czech)

--- languages/cs/main.php
+++ languages/cs/main.php
@@ -1756,6 +1756,11 @@ $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_POLLCREATE'] = '%s může vytvářet ankety 
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_ATTACHMENTS'] = '%s může zaúčtovat přílohy.';
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_DOWNLOAD'] = '%s může stáhnout přílohy.';
 
+// Tooltips for disabled permissions
+$lang['AUTH_TOOLTIP_FORUM_MODE'] = 'Forum mode: %s — cannot be changed';
+$lang['AUTH_TOOLTIP_MOD_OVERRIDE'] = 'Permission enabled via "Moderator"';
+$lang['AUTH_TOOLTIP_GROUP_HAS'] = 'Permission granted by the group';
+
 // Misc
 $lang['SF_SHOW_ON_INDEX'] = 'Zobrazit na hlavní stránce.';
 $lang['SF_PARENT_FORUM'] = 'Mateřské fórum.';
