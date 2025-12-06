5a996510b02dca5116abe1d7bfdf8d15276719cf
Yury Pikhtarev <exileum@icloud.com>
2025-12-07 01:15:54 +0300

    New translations main.php (Russian)

--- languages/ru/main.php
+++ languages/ru/main.php
@@ -142,7 +142,7 @@ $lang['NOTHING_HAS_CHANGED'] = 'Ничего не было изменено';
 
 // Stats block text
 $lang['POSTED_TOPICS_TOTAL'] = 'Наши пользователи создали тем: <b>%s</b>'; // Number of topics
-$lang['POSTED_ARTICLES_TOTAL'] = 'Наши пользователи оставили сообщений: <b>%s</b>'; // Number of posts
+$lang['POSTED_POSTS_TOTAL'] = 'Our users have posted a total of <b>%s</b> posts'; // Number of posts
 $lang['REGISTERED_USERS_TOTAL'] = 'Всего зарегистрированных пользователей: <b>%s</b>'; // # registered users
 $lang['USERS_TOTAL_GENDER'] = 'Парней: <b>%d</b>, Девушек: <b>%d</b>, Не указали: <b>%d</b>';
 $lang['NEWEST_USER'] = 'Последний зарегистрированный пользователь: <b>%s</b>'; // a href, username, /a
