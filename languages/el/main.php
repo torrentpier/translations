6e4520b243d5396d2797ec00192fd0d2d565c5a7
Yury Pikhtarev <exileum@icloud.com>
2025-12-07 01:15:40 +0300

    New translations main.php (Greek)

--- languages/el/main.php
+++ languages/el/main.php
@@ -142,7 +142,7 @@ $lang['NOTHING_HAS_CHANGED'] = 'Τίποτα δεν έχει αλλάξει';
 
 // Stats block text
 $lang['POSTED_TOPICS_TOTAL'] = 'Οι χρήστες μας έχουν δημοσιεύσει συνολικά <b>%s</b> θέματα'; // Number of topics
-$lang['POSTED_ARTICLES_TOTAL'] = 'Οι χρήστες μας έχουν δημοσιεύσει συνολικά <b>%s</b> άρθρα'; // Number of posts
+$lang['POSTED_POSTS_TOTAL'] = 'Our users have posted a total of <b>%s</b> posts'; // Number of posts
 $lang['REGISTERED_USERS_TOTAL'] = 'Έχουμε <b>%s</b> εγγεγραμμένους χρήστες'; // # registered users
 $lang['USERS_TOTAL_GENDER'] = 'Αγόρια: <b>%d</b>, Κορίτσια: <b>%d</b>, Άλλα: <b>%d</b>';
 $lang['NEWEST_USER'] = 'Ο νεότερος εγγεγραμμένος χρήστης είναι <b>%s</b>.'; // a href, username, /a
