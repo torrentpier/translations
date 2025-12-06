b05c46f97b0b9643928955c2169f1ea0c994ec80
Yury Pikhtarev <exileum@icloud.com>
2025-12-07 00:14:22 +0300

    New translations main.php (Hungarian)

--- languages/hu/main.php
+++ languages/hu/main.php
@@ -1757,6 +1757,11 @@ $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_POLLCREATE'] = '%s hozható létre közvéle
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_ATTACHMENTS'] = '%s küldhetnek csatolmányokat';
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_DOWNLOAD'] = '%s letölthető mellékletek';
 
+// Tooltips for disabled permissions
+$lang['AUTH_TOOLTIP_FORUM_MODE'] = 'Forum mode: %s — cannot be changed';
+$lang['AUTH_TOOLTIP_MOD_OVERRIDE'] = 'Permission enabled via "Moderator"';
+$lang['AUTH_TOOLTIP_GROUP_HAS'] = 'Permission granted by the group';
+
 // Misc
 $lang['SF_SHOW_ON_INDEX'] = 'Show-fő oldal';
 $lang['SF_PARENT_FORUM'] = 'Szülői fórum';
