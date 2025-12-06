3deb8fd72a6bf9d5dfa94283e85c788698a63475
Yury Pikhtarev <exileum@icloud.com>
2025-12-07 01:16:19 +0300

    New translations main.php (Chinese Traditional)

--- languages/zh/main.php
+++ languages/zh/main.php
@@ -142,7 +142,7 @@ $lang['NOTHING_HAS_CHANGED'] = '没有任何改动';
 
 // Stats block text
 $lang['POSTED_TOPICS_TOTAL'] = '我们的用户已经一共发布了 <b>%s</b> 个主题'; // Number of topics
-$lang['POSTED_ARTICLES_TOTAL'] = '我们的用户已经一共发布了 <b>%s</b> 篇文章'; // Number of posts
+$lang['POSTED_POSTS_TOTAL'] = 'Our users have posted a total of <b>%s</b> posts'; // Number of posts
 $lang['REGISTERED_USERS_TOTAL'] = '我们有 <b>%s</b> 个注册用户'; // # registered users
 $lang['USERS_TOTAL_GENDER'] = '男性：<b>%d</b>，女性：<b>%d</b>，其他：<b>%d</b>';
 $lang['NEWEST_USER'] = '最新的注册用户是<b>%s</b>'; // a href, username, /a
