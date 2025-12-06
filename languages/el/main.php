02000f20967335470161e9ab2587b06f0402e7b6
Yury Pikhtarev <exileum@icloud.com>
2025-12-07 02:16:08 +0300

    New translations main.php (Greek)

--- languages/el/main.php
+++ languages/el/main.php
@@ -102,7 +102,7 @@ $lang['ADMIN_DISABLE_CRON_TITLE'] = 'Το φόρουμ είναι εκτός λ�
 $lang['ADMIN_UNLOCK'] = 'Ενεργοποίηση φόρουμ.';
 $lang['ADMIN_UNLOCKED'] = 'Ξεκλείδωτο';
 $lang['ADMIN_UNLOCK_CRON'] = 'Αφαιρέστε το κλείδωμα.';
-$lang['PAGE_NOT_FOUND'] = 'Page not found';
+$lang['PAGE_NOT_FOUND'] = 'Η σελίδα δεν βρέθηκε';
 
 $lang['LOADING'] = 'Κατάργηση κλειδώματος';
 $lang['JUMPBOX_TITLE'] = 'Επιλέξτε φόρουμ';
@@ -142,7 +142,7 @@ $lang['NOTHING_HAS_CHANGED'] = 'Τίποτα δεν έχει αλλάξει';
 
 // Stats block text
 $lang['POSTED_TOPICS_TOTAL'] = 'Οι χρήστες μας έχουν δημοσιεύσει συνολικά <b>%s</b> θέματα'; // Number of topics
-$lang['POSTED_POSTS_TOTAL'] = 'Our users have posted a total of <b>%s</b> posts'; // Number of posts
+$lang['POSTED_POSTS_TOTAL'] = 'Οι χρήστες μας έχουν δημοσιεύσει συνολικά <b>%s</b> μηνύματα'; // Number of posts
 $lang['REGISTERED_USERS_TOTAL'] = 'Έχουμε <b>%s</b> εγγεγραμμένους χρήστες'; // # registered users
 $lang['USERS_TOTAL_GENDER'] = 'Αγόρια: <b>%d</b>, Κορίτσια: <b>%d</b>, Άλλα: <b>%d</b>';
 $lang['NEWEST_USER'] = 'Ο νεότερος εγγεγραμμένος χρήστης είναι <b>%s</b>.'; // a href, username, /a
@@ -1757,9 +1757,9 @@ $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_ATTACHMENTS'] = '%s μπορεί να δημ
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_DOWNLOAD'] = '%s μπορεί να κατεβάσει τα συνημμένα.';
 
 // Tooltips for disabled permissions
-$lang['AUTH_TOOLTIP_FORUM_MODE'] = 'Forum mode: %s — cannot be changed';
-$lang['AUTH_TOOLTIP_MOD_OVERRIDE'] = 'Permission enabled via "Moderator"';
-$lang['AUTH_TOOLTIP_GROUP_HAS'] = 'Permission granted by the group';
+$lang['AUTH_TOOLTIP_FORUM_MODE'] = 'Λειτουργία φόρουμ: %s — δεν μπορεί να αλλάξει';
+$lang['AUTH_TOOLTIP_MOD_OVERRIDE'] = 'Άδεια ενεργοποιημένη μέσω "Διαχειριστή"';
+$lang['AUTH_TOOLTIP_GROUP_HAS'] = 'Άδεια που παρέχεται από την ομάδα';
 
 // Misc
 $lang['SF_SHOW_ON_INDEX'] = 'Εμφάνιση στην κεντρική σελίδα.';
