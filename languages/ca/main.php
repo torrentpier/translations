ef422a3d05779f8bd04335292648f999f8f82c62
Yury Pikhtarev <exileum@icloud.com>
2025-12-07 01:15:36 +0300

    New translations main.php (Catalan)

--- languages/ca/main.php
+++ languages/ca/main.php
@@ -142,7 +142,7 @@ $lang['NOTHING_HAS_CHANGED'] = 'Res ha canviat';
 
 // Stats block text
 $lang['POSTED_TOPICS_TOTAL'] = 'Els nostres usuaris han publicat un total de temes <b>%s</b>'; // Number of topics
-$lang['POSTED_ARTICLES_TOTAL'] = 'Els nostres usuaris han publicat un total de <b>%s</b> articles'; // Number of posts
+$lang['POSTED_POSTS_TOTAL'] = 'Our users have posted a total of <b>%s</b> posts'; // Number of posts
 $lang['REGISTERED_USERS_TOTAL'] = 'Hem <b>%s</b> usuaris registrats'; // # registered users
 $lang['USERS_TOTAL_GENDER'] = 'Nois: <b>%d</b>, noies: <b>%d</b>, altres: <b>%d</b>.';
 $lang['NEWEST_USER'] = 'El més nou usuari registrat és <b>%s</b>'; // a href, username, /a
