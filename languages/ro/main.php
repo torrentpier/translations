ecb369f9c89fea959603a951aff9a98ad6237eed
Yury Pikhtarev <exileum@icloud.com>
2025-12-07 00:14:10 +0300

    New translations main.php (Romanian)

--- languages/ro/main.php
+++ languages/ro/main.php
@@ -1756,6 +1756,11 @@ $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_POLLCREATE'] = '%s poate crea chestionarele 
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_ATTACHMENTS'] = '%s pot posta atasamente';
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_DOWNLOAD'] = '%s puteți descărca atașamente';
 
+// Tooltips for disabled permissions
+$lang['AUTH_TOOLTIP_FORUM_MODE'] = 'Forum mode: %s — cannot be changed';
+$lang['AUTH_TOOLTIP_MOD_OVERRIDE'] = 'Permission enabled via "Moderator"';
+$lang['AUTH_TOOLTIP_GROUP_HAS'] = 'Permission granted by the group';
+
 // Misc
 $lang['SF_SHOW_ON_INDEX'] = 'Arătați pe pagina principală.';
 $lang['SF_PARENT_FORUM'] = 'Părinte forum';
