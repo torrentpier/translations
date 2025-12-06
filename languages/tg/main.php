3a3a35307680fc8bb11ce53191bce93ce8e6c81e
Yury Pikhtarev <exileum@icloud.com>
2025-12-07 00:15:10 +0300

    New translations main.php (Tajik)

--- languages/tg/main.php
+++ languages/tg/main.php
@@ -1756,6 +1756,11 @@ $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_POLLCREATE'] = '%s метавонад дар
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_ATTACHMENTS'] = '%s метавонад замимаҳои овехтани.';
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_DOWNLOAD'] = '%s метавонад замимаҳоро зеркашӣ кунад.';
 
+// Tooltips for disabled permissions
+$lang['AUTH_TOOLTIP_FORUM_MODE'] = 'Forum mode: %s — cannot be changed';
+$lang['AUTH_TOOLTIP_MOD_OVERRIDE'] = 'Permission enabled via "Moderator"';
+$lang['AUTH_TOOLTIP_GROUP_HAS'] = 'Permission granted by the group';
+
 // Misc
 $lang['SF_SHOW_ON_INDEX'] = 'Намоиши дар саҳифаи асосӣ.';
 $lang['SF_PARENT_FORUM'] = 'Форуми падару модар.';
