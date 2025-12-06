343ac824717be7762bd8a2b03d9513ca6bbae16f
Yury Pikhtarev <exileum@icloud.com>
2025-12-07 01:15:51 +0300

    New translations main.php (Dutch)

--- languages/nl/main.php
+++ languages/nl/main.php
@@ -142,7 +142,7 @@ $lang['NOTHING_HAS_CHANGED'] = 'Niets is gewijzigd';
 
 // Stats block text
 $lang['POSTED_TOPICS_TOTAL'] = 'Onze gebruikers hebben <b>%s</b> onderwerpen geplaatst.'; // Number of topics
-$lang['POSTED_ARTICLES_TOTAL'] = 'Onze gebruikers hebben <b>%s</b> artikelen geplaatst.'; // Number of posts
+$lang['POSTED_POSTS_TOTAL'] = 'Our users have posted a total of <b>%s</b> posts'; // Number of posts
 $lang['REGISTERED_USERS_TOTAL'] = 'We hebben <b>%s</b> geregistreerde gebruikers'; // # registered users
 $lang['USERS_TOTAL_GENDER'] = 'Jongens: <b>%d</b>, Meisjes: <b>%d</b>, Anderen: <b>%d</b>';
 $lang['NEWEST_USER'] = 'De nieuwste geregistreerde gebruiker is <b>%s</b>'; // a href, username, /a
