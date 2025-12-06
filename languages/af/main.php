a598c3b42096dace8ef16beee3342b0768c8ae50
Yury Pikhtarev <exileum@icloud.com>
2025-12-07 00:14:11 +0300

    New translations main.php (Afrikaans)

--- languages/af/main.php
+++ languages/af/main.php
@@ -1756,6 +1756,11 @@ $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_POLLCREATE'] = '%s kan meningspeilings in hi
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_ATTACHMENTS'] = '%s kan aanhangsels plaas';
 $lang['FORUM_AUTH_LIST_EXPLAIN_AUTH_DOWNLOAD'] = '%s kan aanhangsels aflaai';
 
+// Tooltips for disabled permissions
+$lang['AUTH_TOOLTIP_FORUM_MODE'] = 'Forum mode: %s — cannot be changed';
+$lang['AUTH_TOOLTIP_MOD_OVERRIDE'] = 'Permission enabled via "Moderator"';
+$lang['AUTH_TOOLTIP_GROUP_HAS'] = 'Permission granted by the group';
+
 // Misc
 $lang['SF_SHOW_ON_INDEX'] = 'Wys op hoofblad';
 $lang['SF_PARENT_FORUM'] = 'Ouerforum';
