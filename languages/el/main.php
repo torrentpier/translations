f984cea099508953e78510dc86b7554b22a3630d
Yury Pikhtarev <exileum@icloud.com>
2025-12-15 22:32:03 +0400

    New translations main.php (Greek)

--- languages/el/main.php
+++ languages/el/main.php
@@ -118,8 +118,6 @@ $lang['ONLINE_ADMIN'] = 'Διαχειριστής';
 $lang['ONLINE_MOD'] = 'Συντονιστής';
 $lang['ONLINE_GROUP_MEMBER'] = 'Μέλος της ομάδας';
 
-$lang['CANT_EDIT_IN_DEMO_MODE'] = 'Αυτή η ενέργεια δεν μπορεί να εκτελεστεί σε δοκιμαστική λειτουργία!';
-
 $lang['CURRENT_TIME'] = 'Τρέχουσα ώρα είναι: <span class="tz_time">%s</span>';
 
 $lang['SEARCH_NEW'] = 'Δείτε νεότερες δημοσιεύσεις';
