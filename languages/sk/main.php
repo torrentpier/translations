55e1fd8f79f77e7e30d96da1ca24c70418216598
Yury Pikhtarev <exileum@icloud.com>
2025-12-15 22:32:21 +0400

    New translations main.php (Slovak)

--- languages/sk/main.php
+++ languages/sk/main.php
@@ -118,8 +118,6 @@ $lang['ONLINE_ADMIN'] = 'Administrátor';
 $lang['ONLINE_MOD'] = 'Moderátor';
 $lang['ONLINE_GROUP_MEMBER'] = 'Člen skupiny';
 
-$lang['CANT_EDIT_IN_DEMO_MODE'] = 'Túto akciu nemožno vykonať v ukážkovom režime!';
-
 $lang['CURRENT_TIME'] = 'Aktuálny čas je: <span class="tz_time">%s</span>';
 
 $lang['SEARCH_NEW'] = 'Zobraziť najnovšie príspevky';
