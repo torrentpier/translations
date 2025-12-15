a1f1d87ed42540f877044d0ee3b26f01ab5062a9
Yury Pikhtarev <exileum@icloud.com>
2025-12-15 22:32:46 +0400

    New translations main.php (German)

--- languages/de/main.php
+++ languages/de/main.php
@@ -118,8 +118,6 @@ $lang['ONLINE_ADMIN'] = 'Administrator';
 $lang['ONLINE_MOD'] = 'Moderator';
 $lang['ONLINE_GROUP_MEMBER'] = 'Gruppenmitglied';
 
-$lang['CANT_EDIT_IN_DEMO_MODE'] = 'Diese Aktion kann im Demomodus nicht ausgeführt werden!';
-
 $lang['CURRENT_TIME'] = 'Aktuelle Zeit: <span class="tz_time">%s</span>';
 
 $lang['SEARCH_NEW'] = 'Neueste Beiträge anzeigen';
