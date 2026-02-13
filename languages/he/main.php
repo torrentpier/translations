e8272237c3d8d6edb30f7693fba1664ad4bcda00
Yury Pikhtarev <exileum@icloud.com>
2026-02-13 13:21:31 +0400

    New translations main.php (Hebrew)

--- languages/he/main.php
+++ languages/he/main.php
@@ -284,6 +284,18 @@ $lang['POST_STICKY'] = 'דביק';
 $lang['POST_NORMAL'] = 'נורמלי';
 $lang['POST_DOWNLOAD'] = 'הורד';
 
+// Anonymous posting
+$lang['ANONYMOUS'] = 'Anonymous';
+$lang['ANONYMOUS_TOPIC'] = 'Post topic anonymously';
+$lang['ANONYMOUS_REPLY'] = 'Post reply anonymously';
+$lang['HIDE_AUTHORSHIP'] = 'Post anonymously by default';
+$lang['HIDE_AUTHORSHIP_EXPLAIN'] = 'When enabled, new posts will default to anonymous mode. You can toggle this per post.';
+$lang['ALLOW_ANONYMOUS_POSTING'] = 'Allow anonymous posting';
+$lang['ANONYMOUS_POST_LABEL'] = 'Posted anonymously';
+$lang['POSTED_ANONYMOUSLY'] = 'posted anonymously';
+$lang['ANONYMOUS_ADMIN_NOTICE'] = 'Admins and moderators can see the real author';
+$lang['YOU'] = 'You';
+
 $lang['PRINT_PAGE'] = 'הדפס דף';
 
 $lang['CONFIRM_DELETE'] = 'אתה בטוח שאתה רוצה למחוק הודעה זו?';
