c7ac8667c7c9d9559d2cad0c165c32b4c062fe03
Yury Pikhtarev <exileum@icloud.com>
2026-02-13 13:21:33 +0400

    New translations main.php (Hungarian)

--- languages/hu/main.php
+++ languages/hu/main.php
@@ -284,6 +284,18 @@ $lang['POST_STICKY'] = 'Ragadós';
 $lang['POST_NORMAL'] = 'Normál';
 $lang['POST_DOWNLOAD'] = 'Letöltés';
 
+// Anonymous posting
+$lang['ANONYMOUS'] = 'Anonymous';
+$lang['ANONYMOUS_TOPIC'] = 'Post topic anonymously';
+$lang['ANONYMOUS_REPLY'] = 'Post reply anonymously';
+$lang['HIDE_AUTHORSHIP'] = 'Post anonymously by default';
+$lang['HIDE_AUTHORSHIP_EXPLAIN'] = 'When enabled, new posts will default to anonymous mode. You can toggle this per post.';
+$lang['ALLOW_ANONYMOUS_POSTING'] = 'Allow anonymous posting';
+$lang['ANONYMOUS_POST_LABEL'] = 'Posted anonymously';
+$lang['POSTED_ANONYMOUSLY'] = 'posted anonymously';
+$lang['ANONYMOUS_ADMIN_NOTICE'] = 'Admins and moderators can see the real author';
+$lang['YOU'] = 'You';
+
 $lang['PRINT_PAGE'] = 'Oldal nyomtatása';
 
 $lang['CONFIRM_DELETE'] = 'Biztos, hogy törölni akarja ezt a bejegyzést?';
