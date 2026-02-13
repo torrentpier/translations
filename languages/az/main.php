cf38e33900107526e7bc60caec0f08697be6f0f6
Yury Pikhtarev <exileum@icloud.com>
2026-02-13 13:22:20 +0400

    New translations main.php (Azerbaijani)

--- languages/az/main.php
+++ languages/az/main.php
@@ -284,6 +284,18 @@ $lang['POST_STICKY'] = 'Yapışan';
 $lang['POST_NORMAL'] = 'Normal';
 $lang['POST_DOWNLOAD'] = 'Yüklə';
 
+// Anonymous posting
+$lang['ANONYMOUS'] = 'Anonymous';
+$lang['ANONYMOUS_TOPIC'] = 'Post topic anonymously';
+$lang['ANONYMOUS_REPLY'] = 'Post reply anonymously';
+$lang['HIDE_AUTHORSHIP'] = 'Post anonymously by default';
+$lang['HIDE_AUTHORSHIP_EXPLAIN'] = 'When enabled, new posts will default to anonymous mode. You can toggle this per post.';
+$lang['ALLOW_ANONYMOUS_POSTING'] = 'Allow anonymous posting';
+$lang['ANONYMOUS_POST_LABEL'] = 'Posted anonymously';
+$lang['POSTED_ANONYMOUSLY'] = 'posted anonymously';
+$lang['ANONYMOUS_ADMIN_NOTICE'] = 'Admins and moderators can see the real author';
+$lang['YOU'] = 'You';
+
 $lang['PRINT_PAGE'] = 'Səhifəni çap edin';
 
 $lang['CONFIRM_DELETE'] = 'Siz bu postu silmək istədiyinizdən əminsiniz?';
