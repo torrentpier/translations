d665d40f732580f89f39b33cc4132f1771acb453
Yury Pikhtarev <exileum@icloud.com>
2026-02-15 02:23:18 +0400

    New Crowdin updates (#53)
    * New translations main.php (Romanian)
    
    * New translations main.php (French)
    
    * New translations main.php (Spanish)
    
    * New translations main.php (Afrikaans)
    
    * New translations main.php (Arabic)
    
    * New translations main.php (Belarusian)
    
    * New translations main.php (Bulgarian)
    
    * New translations main.php (Catalan)
    
    * New translations main.php (Czech)
    
    * New translations main.php (Danish)
    
    * New translations main.php (German)
    
    * New translations main.php (Greek)
    
    * New translations main.php (Finnish)
    
    * New translations main.php (Hebrew)
    
    * New translations main.php (Hungarian)
    
    * New translations main.php (Armenian)
    
    * New translations main.php (Italian)
    
    * New translations main.php (Japanese)
    
    * New translations main.php (Georgian)
    
    * New translations main.php (Korean)
    
    * New translations main.php (Lithuanian)
    
    * New translations main.php (Dutch)
    
    * New translations main.php (Norwegian)
    
    * New translations main.php (Polish)
    
    * New translations main.php (Russian)
    
    * New translations main.php (Slovak)
    
    * New translations main.php (Slovenian)
    
    * New translations main.php (Albanian)
    
    * New translations main.php (Serbian (Cyrillic))
    
    * New translations main.php (Swedish)
    
    * New translations main.php (Turkish)
    
    * New translations main.php (Ukrainian)
    
    * New translations main.php (Chinese Traditional)
    
    * New translations main.php (English)
    
    * New translations main.php (Vietnamese)
    
    * New translations main.php (Portuguese, Brazilian)
    
    * New translations main.php (Indonesian)
    
    * New translations main.php (Thai)
    
    * New translations main.php (Croatian)
    
    * New translations main.php (Kazakh)
    
    * New translations main.php (Estonian)
    
    * New translations main.php (Latvian)
    
    * New translations main.php (Azerbaijani)
    
    * New translations main.php (Hindi)
    
    * New translations main.php (Bosnian)
    
    * New translations main.php (Uzbek)
    
    * New translations main.php (Tajik)
    
    * New translations main.php (Romanian)
    
    * New translations main.php (French)
    
    * New translations main.php (Spanish)
    
    * New translations main.php (Afrikaans)
    
    * New translations main.php (Arabic)
    
    * New translations main.php (Belarusian)
    
    * New translations main.php (Bulgarian)
    
    * New translations main.php (Catalan)
    
    * New translations main.php (Czech)
    
    * New translations main.php (Danish)
    
    * New translations main.php (German)
    
    * New translations main.php (Greek)
    
    * New translations main.php (Finnish)
    
    * New translations main.php (Hebrew)
    
    * New translations main.php (Hungarian)
    
    * New translations main.php (Armenian)
    
    * New translations main.php (Italian)
    
    * New translations main.php (Japanese)
    
    * New translations main.php (Georgian)
    
    * New translations main.php (Korean)
    
    * New translations main.php (Lithuanian)
    
    * New translations main.php (Dutch)
    
    * New translations main.php (Norwegian)
    
    * New translations main.php (Polish)
    
    * New translations main.php (Russian)
    
    * New translations main.php (Slovak)
    
    * New translations main.php (Slovenian)
    
    * New translations main.php (Albanian)
    
    * New translations main.php (Serbian (Cyrillic))
    
    * New translations main.php (Swedish)
    
    * New translations main.php (Turkish)
    
    * New translations main.php (Ukrainian)
    
    * New translations main.php (Chinese Traditional)
    
    * New translations main.php (English)
    
    * New translations main.php (Vietnamese)
    
    * New translations main.php (Portuguese, Brazilian)
    
    * New translations main.php (Indonesian)
    
    * New translations main.php (Thai)
    
    * New translations main.php (Croatian)
    
    * New translations main.php (Kazakh)
    
    * New translations main.php (Estonian)
    
    * New translations main.php (Latvian)
    
    * New translations main.php (Azerbaijani)
    
    * New translations main.php (Hindi)
    
    * New translations main.php (Bosnian)
    
    * New translations main.php (Uzbek)
    
    * New translations main.php (Tajik)

--- languages/nl/main.php
+++ languages/nl/main.php
@@ -2314,5 +2314,27 @@ $lang['RELEASER_STAT_SIZE'] = 'Totale grootte:';
 $lang['RELEASER_STAT'] = 'Releaser-statistieken:';
 $lang['RELEASER_STAT_SHOW'] = 'Toon statistieken';
 
+// Spam protection
+$lang['REGISTRATION_DENIED'] = 'Registration denied. Please contact the administrator if you believe this is an error.';
+$lang['POST_SPAM_DENIED'] = 'Your message has been identified as spam and cannot be posted.';
+$lang['PM_SPAM_DENIED'] = 'Your message has been identified as spam and cannot be sent.';
+
+// Spam log (admin)
+$lang['SPAM_LOG'] = 'Spam Log';
+$lang['SPAM_LOG_DESC'] = 'This log records all registration attempts and content submissions that were flagged by the spam protection system. <b>Denied</b> &mdash; the action was completely blocked (registration rejected, post/PM not sent). <b>Moderated</b> &mdash; for registrations, the account was created but requires email activation; for content, the post/PM was allowed through but flagged as suspicious.';
+$lang['SPAM_LOG_CHECK_TYPE'] = 'Check type';
+$lang['SPAM_LOG_CHECK_TYPE_DESC_REG'] = 'registration &mdash; new account signup';
+$lang['SPAM_LOG_CHECK_TYPE_DESC_CONTENT'] = 'content &mdash; post or private message';
+$lang['SPAM_LOG_DECISION'] = 'Decision';
+$lang['SPAM_LOG_PROVIDER'] = 'Provider';
+$lang['SPAM_LOG_REASON'] = 'Reden';
+$lang['SPAM_LOG_RESPONSE_TIME'] = 'Time (ms)';
+$lang['SPAM_LOG_ALL'] = 'Alle';
+$lang['SPAM_LOG_DETAILS'] = 'Details';
+$lang['SPAM_LOG_NO_RECORDS'] = 'No spam log entries found';
+$lang['SPAM_LOG_TOTAL'] = 'Total entries';
+$lang['SPAM_LOG_DENIED_COUNT'] = 'Denied';
+$lang['SPAM_LOG_MODERATED_COUNT'] = 'Moderated';
+
 // Dark mode
 $lang['DARK_MODE_TOGGLE'] = 'Schakel donkere modus in';
