44808e54a598f762423a67a308eacf29caa6eb00
Yury Pikhtarev <exileum@icloud.com>
2026-02-15 02:28:18 +0400

    New translations main.php (Armenian)

--- languages/hy/main.php
+++ languages/hy/main.php
@@ -2315,26 +2315,26 @@ $lang['RELEASER_STAT'] = 'Հետևողի վիճակագրությունը:';
 $lang['RELEASER_STAT_SHOW'] = 'Ցույց տալ վիճակագրությունը';
 
 // Spam protection
-$lang['REGISTRATION_DENIED'] = 'Registration denied. Please contact the administrator if you believe this is an error.';
-$lang['POST_SPAM_DENIED'] = 'Your message has been identified as spam and cannot be posted.';
-$lang['PM_SPAM_DENIED'] = 'Your message has been identified as spam and cannot be sent.';
+$lang['REGISTRATION_DENIED'] = 'Գրանցումը մերժվել է։ Խնդրում ենք դիմել ադմինիստրատորին, եթե կարծում եք, թե սա սխալ է։';
+$lang['POST_SPAM_DENIED'] = 'Ձեր հաղորդագրությունը ճանաչվել է որպես սպամ և չի կարող հրապարակվել։';
+$lang['PM_SPAM_DENIED'] = 'Ձեր հաղորդագրությունը ճանաչվել է որպես սպամ և չի կարող ուղարկվել։';
 
 // Spam log (admin)
-$lang['SPAM_LOG'] = 'Spam Log';
-$lang['SPAM_LOG_DESC'] = 'This log records all registration attempts and content submissions that were flagged by the spam protection system. <b>Denied</b> &mdash; the action was completely blocked (registration rejected, post/PM not sent). <b>Moderated</b> &mdash; for registrations, the account was created but requires email activation; for content, the post/PM was allowed through but flagged as suspicious.';
-$lang['SPAM_LOG_CHECK_TYPE'] = 'Check type';
-$lang['SPAM_LOG_CHECK_TYPE_DESC_REG'] = 'registration &mdash; new account signup';
-$lang['SPAM_LOG_CHECK_TYPE_DESC_CONTENT'] = 'content &mdash; post or private message';
-$lang['SPAM_LOG_DECISION'] = 'Decision';
-$lang['SPAM_LOG_PROVIDER'] = 'Provider';
+$lang['SPAM_LOG'] = 'Սպամի արխիվը';
+$lang['SPAM_LOG_DESC'] = 'Այս արխիվում գրանցվում են բոլոր այն գրանցման փորձերը և բովանդակության ներկայացումները, որոնք ազդանշանվել են սպամ պաշտպանությամբ։ <b>Մերժված</b> &mdash; գործողությունը լրիվ արգելվել է (գրանցումը մերժվել է, գրառումը/անձնական հաղորդագրությունը չի ուղարկվել)։ <b>Մոդերացված</b> &mdash; գրանցման դեպքում հաշիվը ստեղծվել է, սակայն պահանջում է էլփոստի ակտիվացում; բովանդակության դեպքում գրառումը/անձնական հաղորդագրությունը թույլատրվել է, բայց նշվել է որպես կասկածելի։';
+$lang['SPAM_LOG_CHECK_TYPE'] = 'Ստուգման տեսակ';
+$lang['SPAM_LOG_CHECK_TYPE_DESC_REG'] = 'գրանցում &mdash; նոր հաշվի գրանցում';
+$lang['SPAM_LOG_CHECK_TYPE_DESC_CONTENT'] = 'բովանդակություն &mdash; գրառում կամ անձնական հաղորդագրություն';
+$lang['SPAM_LOG_DECISION'] = 'Որոշում';
+$lang['SPAM_LOG_PROVIDER'] = 'Ապահովող';
 $lang['SPAM_LOG_REASON'] = 'Պատճառը';
-$lang['SPAM_LOG_RESPONSE_TIME'] = 'Time (ms)';
+$lang['SPAM_LOG_RESPONSE_TIME'] = 'Ժամանակը (մվ)';
 $lang['SPAM_LOG_ALL'] = 'Բոլոր';
-$lang['SPAM_LOG_DETAILS'] = 'Details';
-$lang['SPAM_LOG_NO_RECORDS'] = 'No spam log entries found';
-$lang['SPAM_LOG_TOTAL'] = 'Total entries';
-$lang['SPAM_LOG_DENIED_COUNT'] = 'Denied';
-$lang['SPAM_LOG_MODERATED_COUNT'] = 'Moderated';
+$lang['SPAM_LOG_DETAILS'] = 'Մանրամասներ';
+$lang['SPAM_LOG_NO_RECORDS'] = 'Սպամի արխիվում գրանցումներ չեն գտնվել';
+$lang['SPAM_LOG_TOTAL'] = 'Ընդհանուր ցուցումներ';
+$lang['SPAM_LOG_DENIED_COUNT'] = 'Մերժված';
+$lang['SPAM_LOG_MODERATED_COUNT'] = 'Մոդերացված';
 
 // Dark mode
 $lang['DARK_MODE_TOGGLE'] = 'Ընտրել մութ ռեժիմը';
