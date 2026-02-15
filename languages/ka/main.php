bba6276d4d261d2ad491d391a615be6f1392e399
Yury Pikhtarev <exileum@icloud.com>
2026-02-15 21:38:56 +0400

    New translations main.php (Georgian)

--- languages/ka/main.php
+++ languages/ka/main.php
@@ -2336,5 +2336,30 @@ $lang['SPAM_LOG_TOTAL'] = 'სულ ჩანაწერების რაო
 $lang['SPAM_LOG_DENIED_COUNT'] = 'უარი';
 $lang['SPAM_LOG_MODERATED_COUNT'] = 'მენეჯმენტი';
 
+// Marketplace / Modifications
+$lang['MARKETPLACE'] = 'Marketplace';
+$lang['MODIFICATIONS_LIST'] = 'ცვლილებები';
+$lang['MODS_VERSION'] = 'მოდელი.';
+$lang['MODS_DOWNLOADS'] = 'გადმოწერა';
+$lang['MODS_RATING'] = 'Rating';
+$lang['MODS_ACTIONS'] = 'Actions';
+$lang['MODS_AUTHOR'] = 'ავტორი';
+$lang['MODS_CATEGORY'] = 'კატეგორია';
+$lang['MODS_LAST_UPDATED'] = 'ბოლო განახლება';
+$lang['MODS_TOTAL'] = 'საერთო';
+$lang['MODS_NO_RESOURCES'] = 'No modifications found.';
+$lang['MODS_INSTALL'] = 'Install';
+$lang['MODS_INSTALL_SOON'] = 'Installation feature coming soon';
+$lang['MODS_ALL_CATEGORIES'] = 'All categories';
+$lang['MODS_FREE'] = 'Free';
+$lang['MODS_PAID'] = 'Paid';
+$lang['MODS_THEMES'] = 'Themes';
+$lang['MODS_LANGPACKS'] = 'Language Packs';
+$lang['MODS_API_ERROR'] = 'Could not load modifications: %s';
+$lang['MODS_API_UNAVAILABLE'] = 'The modifications server is temporarily unavailable.';
+$lang['MODS_DESCRIPTION'] = 'This page lists all resources published on the official TorrentPier forum. One-click installation will be available in a future update — for now, use this page as a resource showcase.';
+$lang['MODS_REVIEW'] = 'review';
+$lang['MODS_REVIEWS'] = 'reviews';
+
 // Dark mode
 $lang['DARK_MODE_TOGGLE'] = 'მენიუს ღია გამოკვლევა переключение';
