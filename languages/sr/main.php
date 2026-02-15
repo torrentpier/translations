affa26c3f70f0c1115c27511c0ce62a8be76d9b6
Yury Pikhtarev <exileum@icloud.com>
2026-02-16 03:56:10 +0400

    New translations main.php (Serbian (Cyrillic))

--- languages/sr/main.php
+++ languages/sr/main.php
@@ -2337,29 +2337,29 @@ $lang['SPAM_LOG_DENIED_COUNT'] = 'Одбијено';
 $lang['SPAM_LOG_MODERATED_COUNT'] = 'Модерирано';
 
 // Marketplace / Modifications
-$lang['MARKETPLACE'] = 'Marketplace';
+$lang['MARKETPLACE'] = 'Маркетплејс';
 $lang['MODIFICATIONS_LIST'] = 'Модификације';
 $lang['MODS_VERSION'] = 'Верзија';
 $lang['MODS_DOWNLOADS'] = 'Преузимања';
-$lang['MODS_RATING'] = 'Rating';
-$lang['MODS_ACTIONS'] = 'Actions';
+$lang['MODS_RATING'] = 'Рејтинг';
+$lang['MODS_ACTIONS'] = 'Акције';
 $lang['MODS_AUTHOR'] = 'Аутор';
 $lang['MODS_CATEGORY'] = 'Категорија';
 $lang['MODS_LAST_UPDATED'] = 'Последње ажурирање.';
 $lang['MODS_TOTAL'] = 'Укупна';
-$lang['MODS_NO_RESOURCES'] = 'No modifications found.';
-$lang['MODS_INSTALL'] = 'Install';
-$lang['MODS_INSTALL_SOON'] = 'Installation feature coming soon';
-$lang['MODS_ALL_CATEGORIES'] = 'All categories';
-$lang['MODS_FREE'] = 'Free';
-$lang['MODS_PAID'] = 'Paid';
-$lang['MODS_THEMES'] = 'Themes';
-$lang['MODS_LANGPACKS'] = 'Language Packs';
-$lang['MODS_API_ERROR'] = 'Could not load modifications: %s';
-$lang['MODS_API_UNAVAILABLE'] = 'The modifications server is temporarily unavailable.';
-$lang['MODS_DESCRIPTION'] = 'This page lists all resources published on the official TorrentPier forum. One-click installation will be available in a future update — for now, use this page as a resource showcase.';
-$lang['MODS_REVIEW'] = 'review';
-$lang['MODS_REVIEWS'] = 'reviews';
+$lang['MODS_NO_RESOURCES'] = 'Ниједна модификација није пронађена.';
+$lang['MODS_INSTALL'] = 'Инсталирај';
+$lang['MODS_INSTALL_SOON'] = 'Опција инсталирања долази ускоро';
+$lang['MODS_ALL_CATEGORIES'] = 'Све категорије';
+$lang['MODS_FREE'] = 'Бесплатно';
+$lang['MODS_PAID'] = 'Плаћено';
+$lang['MODS_THEMES'] = 'Теме';
+$lang['MODS_LANGPACKS'] = 'Језички пакети';
+$lang['MODS_API_ERROR'] = 'Није било могуће учитати модификације: %s';
+$lang['MODS_API_UNAVAILABLE'] = 'Сервер за модификације је тренутно недоступан.';
+$lang['MODS_DESCRIPTION'] = 'Ова страница приказује све ресурсе објављене на званичном форуму TorrentPier-а. Инсталација једним кликом биће доступна у будућем ажурирању — за сада користите ову страницу као изложбу ресурса.';
+$lang['MODS_REVIEW'] = 'преглед';
+$lang['MODS_REVIEWS'] = 'прегледа';
 
 // Dark mode
 $lang['DARK_MODE_TOGGLE'] = 'Прекини тамни режим';
