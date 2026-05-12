bff87c3d7e272e669e49c4a00912d87e4c69c303
Yury Pikhtarev <exileum@icloud.com>
2026-05-13 00:25:39 +0400

    New translations main.php (Azerbaijani)
    [ci skip]

--- languages/az/main.php
+++ languages/az/main.php
@@ -1732,8 +1732,8 @@ $lang['CLICK_RETURN_DISALLOWADMIN'] = 'Qadağan edilmiş istifadəçi adı idar�
 // Version Check
 $lang['VERSION_INFORMATION'] = 'Məlumat Versiyası';
 $lang['CHANGELOG'] = 'Dəyişikliklər';
-$lang['EOL_PROJECT_CLOSED'] = 'Project closed';
-$lang['EOL_FORUM_ARCHIVE'] = 'Forum archive';
+$lang['EOL_PROJECT_CLOSED'] = 'Layihə bağlandı';
+$lang['EOL_FORUM_ARCHIVE'] = 'Forum arxivi';
 
 // Login attempts configuration
 $lang['MAX_LOGIN_ATTEMPTS'] = 'İcazə verilmiş giriş cəhdləri.';
