d9729ae39fd5f56f6c53a3a9f2ab8397d34ba2cf
Yury Pikhtarev <exileum@icloud.com>
2026-05-13 00:12:00 +0400

    New translations main.php (Belarusian)
    [ci skip]

--- languages/be/main.php
+++ languages/be/main.php
@@ -1731,8 +1731,9 @@ $lang['CLICK_RETURN_DISALLOWADMIN'] = 'Націсніце %sHere%s, каб вя�
 
 // Version Check
 $lang['VERSION_INFORMATION'] = 'Інфармацыя аб версіі.';
-$lang['UPDATE_AVAILABLE'] = 'Даступнае абнаўленне';
 $lang['CHANGELOG'] = 'Журнал зменаў';
+$lang['EOL_PROJECT_CLOSED'] = 'Project closed';
+$lang['EOL_FORUM_ARCHIVE'] = 'Forum archive';
 
 // Login attempts configuration
 $lang['MAX_LOGIN_ATTEMPTS'] = 'Дазволеных спробаў ўваходу';
@@ -2335,31 +2336,6 @@ $lang['SPAM_LOG_NO_RECORDS'] = 'Запісы журнала спаму не зн
 $lang['SPAM_LOG_TOTAL'] = 'Усяго запісаў';
 $lang['SPAM_LOG_DENIED_COUNT'] = 'Адхілена';
 $lang['SPAM_LOG_MODERATED_COUNT'] = 'Прадаглядаецца';
-
-// Marketplace / Modifications
-$lang['MARKETPLACE'] = 'Рынак';
-$lang['MODIFICATIONS_LIST'] = 'Мадыфікацыі';
-$lang['MODS_VERSION'] = 'Версія';
-$lang['MODS_DOWNLOADS'] = 'Загрузак';
-$lang['MODS_RATING'] = 'Рэйтынг';
-$lang['MODS_ACTIONS'] = 'Дзеянні';
-$lang['MODS_AUTHOR'] = 'Аўтар';
-$lang['MODS_CATEGORY'] = 'Катэгорыя';
-$lang['MODS_LAST_UPDATED'] = 'Апошняе абнаўленне.';
-$lang['MODS_TOTAL'] = 'Агульная';
-$lang['MODS_NO_RESOURCES'] = 'Мадыфікацыі не знойдзены.';
-$lang['MODS_INSTALL'] = 'Усталяваць';
-$lang['MODS_INSTALL_SOON'] = 'Функцыя ўсталёўкі хутка з\'явіцца';
-$lang['MODS_ALL_CATEGORIES'] = 'Усе катэгорыі';
-$lang['MODS_FREE'] = 'Бясплатна';
-$lang['MODS_PAID'] = 'Платна';
-$lang['MODS_THEMES'] = 'Тэмы';
-$lang['MODS_LANGPACKS'] = 'Моўныя пакеты';
-$lang['MODS_API_ERROR'] = 'Немагчыма загрузіць мадыфікацыі: %s';
-$lang['MODS_API_UNAVAILABLE'] = 'Сервер мадыфікацый часова недаступны.';
-$lang['MODS_DESCRIPTION'] = 'Гэтая старонка ўтрымлівае ўсе рэсурсы, апублікаваныя на афіцыйным форуме TorrentPier. Усталёўка ў адзін клік будзе даступная ў будучым абнаўленні — пакуль выкарыстоўвайце гэтую старонку як выставу рэсурсаў.';
-$lang['MODS_REVIEW'] = 'водгук';
-$lang['MODS_REVIEWS'] = 'водгукі';
 
 // Two-factor authentication
 $lang['TWO_FACTOR_AUTH'] = 'Двухкрокавая верыфікацыя';
